fd918cf06bfed06fffe38906d5f4a5e9062fec3e
Sundaday <john.koch.pro@gmail.com>
2022-03-28 15:50:21 +0200

    init pizza model

--- pizza_mama/Models/Pizza.cs
+++ pizza_mama/Models/Pizza.cs
@@ -2,5 +2,10 @@
 {
     public class Pizza
     {
+        public int PizzaId { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+        public bool vegan { get; set; }
+        public string ingredients { get; set; }
     }
 }
