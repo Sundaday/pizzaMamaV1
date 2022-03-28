2545180d7bfbb11c31d9a64ab5e3f1325baa5d26
Sundaday <john.koch.pro@gmail.com>
2022-03-28 17:14:48 +0200

    clean again + Display annotation

--- pizza_mama/Models/Pizza.cs
+++ pizza_mama/Models/Pizza.cs
@@ -1,11 +1,17 @@
-﻿namespace pizza_mama.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_mama.Models
 {
     public class Pizza
     {
         public int PizzaId { get; set; }
+        [Display(Name = "Nom")]
         public string name { get; set; }
+        [Display(Name = "Price")]
         public float price { get; set; }
+        [Display(Name = "Veggy")]
         public bool vegan { get; set; }
+        [Display(Name = "Ingredients")]
         public string ingredients { get; set; }
     }
 }
