493e16766d8f54056ebe9f4b71ef2df74e0a36a3
Sundaday <john.koch.pro@gmail.com>
2022-03-28 21:03:36 +0200

    add Redirect OnGet()

--- pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -10,8 +10,12 @@ namespace pizza_mama.Pages.Admin
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public IActionResult OnGet(){
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Redirect("/Admin/Pizzas");
+            }
+            return Page();
         }
         public async Task<IActionResult> OnPost(string username, string password, string ReturnUrl)
         {
