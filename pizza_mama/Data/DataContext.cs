93198899ce68f3b215fcbfad465a9c8a5d71db45
Sundaday <john.koch.pro@gmail.com>
2022-03-28 15:56:35 +0200

    init DataContext

---
+++ pizza_mama/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pizza_mama.Models;
+
+namespace pizza_mama.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
