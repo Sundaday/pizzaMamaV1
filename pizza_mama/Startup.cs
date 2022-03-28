490f37b2f516b17d025131fc9b03a67d9835f83f
Sundaday <john.koch.pro@gmail.com>
2022-03-28 17:39:35 +0200

    init cookie auth

--- pizza_mama/Startup.cs
+++ pizza_mama/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,8 +26,12 @@ namespace pizza_mama
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+                options.LoginPath = "/Admin");
+
             services.AddDbContext<DataContext>(options => 
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddRazorPages();
         }
 
@@ -49,6 +54,7 @@ namespace pizza_mama
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
