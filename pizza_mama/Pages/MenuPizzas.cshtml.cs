ac4ef8581089ac2181aca9a9fcfa9d41520f811c
Sundaday <john.koch.pro@gmail.com>
2022-03-29 18:46:19 +0200

    add MenuPizzas html

---
+++ pizza_mama/Pages/MenuPizzas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pizza_mama.Pages
+{
+    public class MenuPizzasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
