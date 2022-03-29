b09f6932d508c48f2cf587dac9d5270e42eab60e
Sundaday <john.koch.pro@gmail.com>
2022-03-29 19:19:56 +0200

    init DataContext MenuPizzas + display html

--- pizza_mama/Pages/MenuPizzas.cshtml.cs
+++ pizza_mama/Pages/MenuPizzas.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_mama.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace pizza_mama.Pages
 {
     public class MenuPizzasModel : PageModel
     {
-        public void OnGet()
+        private readonly pizza_mama.Data.DataContext _context;
+
+        public MenuPizzasModel(pizza_mama.Data.DataContext context)
         {
+            _context = context;
+        }
+
+        public IList<Pizza> Pizza { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Pizza = await _context.Pizzas.ToListAsync();
         }
     }
 }
