2b0af08550e9aca3bc727b5e1984bc866bcadf1f
Sundaday <john.koch.pro@gmail.com>
2022-03-30 18:39:25 +0200

    add cultureInfo & decimal to price

--- pizza_mama/Startup.cs
+++ pizza_mama/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using pizza_mama.Data;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -38,6 +39,11 @@ namespace pizza_mama
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var cultureInfo = new CultureInfo("fr-FR");
+            cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
