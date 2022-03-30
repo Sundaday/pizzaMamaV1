97bba2ec5a9ab639e797ec2f7c80099d3df793f1
Sundaday <john.koch.pro@gmail.com>
2022-03-30 20:20:03 +0200

    init apiControllers

--- pizza_mama/Startup.cs
+++ pizza_mama/Startup.cs
@@ -34,6 +34,7 @@ namespace pizza_mama
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
